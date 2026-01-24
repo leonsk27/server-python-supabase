90b13dca5cc8e3bf804d52bf265111cb3df02c2d
leonsk27 <carlos.ojeda.a.v@gmail.com>
2026-01-24 17:48:32 -0400

    fix settings

--- core/settings.py
+++ core/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    "rest_framework_fwt",
+    "rest_framework_simplejwt",
     "properties",
 ]
 
