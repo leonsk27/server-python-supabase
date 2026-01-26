ee72dfbb92410169a83f576d3a0c75d2dda51bcb
leonsk27 <carlos.ojeda.a.v@gmail.com>
2026-01-25 21:48:01 -0400

    fix syntaxis serializer

--- properties/serializers/prediction_slzer.py
+++ properties/serializers/prediction_slzer.py
@@ -2,5 +2,5 @@ from rest_framework import serializers
 from properties.models.prediction import PredictionHistory
 class PredictionHistorySerializer(serializers.ModelSerializer):
     class Meta:
-        model: PredictionHistory
-        fields: "__all__"
+        model = PredictionHistory
+        fields = "__all__"
